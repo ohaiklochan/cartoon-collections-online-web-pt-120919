0877d292de27f7d6e656e4d888c49ff90074fee6
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 00:36:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,3 +6,28 @@ def roll_call_dwarves(array)
   end
 end
 
+def summon_captain_planet(array)
+    array.collect do |call| 
+    call.capitalize + "!"
+  end
+
+end
+
+def long_planteer_calls(long_planteer_calls)# code an argument here
+  # Your code here
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
+end
+
+def find_the_cheese(cheese)# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
+end
