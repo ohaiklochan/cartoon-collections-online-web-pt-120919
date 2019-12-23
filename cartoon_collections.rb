7c9c02042a379072fd25461a2d3c88ac3608c104
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 00:41:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,4 +13,9 @@ end
 
 def long_planeteer_calls(array)
   i = 0
-  if array.any? {|i|. i.length
+  if array.any? {|i| i.length > 4}
+    false
+  else array.any? {|i| i.length < 4}
+    true
+  end
+end
