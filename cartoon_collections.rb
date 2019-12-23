7b937ee2c9f8ca5e64a387cde2380a7211418fae
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 00:46:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  array.each_with_index do |name, i| 
+    puts "#{i + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+    array.collect do |call| 
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0
+  if array.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+  i = i + 1
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |kind|
+    cheese_types.include? (kind)
+  end
 end
+
