d56b617369d61d6c42bc49397ef5e09d0e0b6824
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 00:32:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,17 +2,8 @@ def roll_call_dwarves(array)
   i = 0
   array = []
   array.each_with_index
-    puts "#{i + 1}. #{array(i)}."
+    puts {|name, index| puts "#{index + 1}. #{name} "}
     i += 1
   end
 end
 
-def summon_captain_planet(array)
-end
-
-def long_planeteer_calls(array)
-end
-
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
