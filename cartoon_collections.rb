bbd16b75fb3ef96ac4bb3218aaf498ceb813c1ea
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 00:28:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,12 +5,4 @@ def roll_call_dwarves(array)
   i += 1
 end
 
-def summon_captain_planet(array)
-end
 
-def long_planeteer_calls(array)
-end
-
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
