f37b888804b4f1e1dc015754920d722c52b5c459
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 00:32:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   i = 0
-  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
+  array.each_with_index {|name, i| puts "#{i + 1}. #{name} "}
   end
 end
 
